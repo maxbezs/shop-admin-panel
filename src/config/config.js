e6c8b33215ae4709fe5b6eb3147b55a085f91bc4
maxbezs <baks.bezsmertnuy@gmail.com>
2024-03-22 22:40:36 +0100

    1

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyAXMQPlGkL8wHxyc1EgpNLvSBRQt9qeoaU",
+  authDomain: "e-commerce-82915.firebaseapp.com",
+  projectId: "e-commerce-82915",
+  storageBucket: "e-commerce-82915.appspot.com",
+  messagingSenderId: "405556393687",
+  appId: "1:405556393687:web:983287b20543317fa32292",
+  measurementId: "G-GPV5K8PGRF",
 };
 
 const app = initializeApp(firebaseConfig);
